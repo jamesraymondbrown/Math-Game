abdff7c1475c40d9e9c6913eb434e953c91f47c3
James Brown <jamesraymondbrown@gmail.com>
2023-01-23 21:18:41 +0000

    make GameFlow class call the Question class, to populate questions

--- game_flow.rb
+++ game_flow.rb
@@ -1,29 +1,97 @@
 require_relative 'questions'
 require_relative 'players'
+require_relative 'question_class'
 
-
-
-def start
-  puts "Welcome to the math game!"
-  puts "Player 1, you're up first. Are you ready?"
-  puts @questions[0][:question]
-
-  print "> "
-  choice = $stdin.gets.chomp.to_s
-
-  if choice == @questions[0][:answer]
-    puts "Great job!"
-    puts "P1: " + player_1.lives.to_s + "/3, P2: " + player_2.lives.to_s + "/3"
-  else
-    puts "Oh no! You got it wrong :( Now you lose a life 🔪"
-    # player_1.lose_life
-    puts "P1: " + player_1.lives.to_s + "/3, P2: " + player_2.lives.to_s + "/3"
+class GameFlow
+  
+  def initialize
+    @player_1 = Player.new("Player_1", "P1")
+    @player_2 = Player.new("Player_2", "P2")
   end
+
+  def start
+    puts "Welcome to the math game!"
+    puts "Player 1, you're up first. Are you ready?"
+    question = Question.new
+
+    puts question.q_string
+
+    print "> "
+    choice = $stdin.gets.chomp.to_s
+
+    if choice == question.answer
+      puts "Great job!"
+      puts "P1: " + @player_1.lives.to_s + "/3, P2: " + @player_2.lives.to_s + "/3"
+    else
+      puts "Oh no! You got it wrong :( Now you lose a life 🔪"
+      @player_1.lose_life
+      puts "P1: " + @player_1.lives.to_s + "/3, P2: " + @player_2.lives.to_s + "/3"
+    end
+
+    if @player_1.lives.to_s == "0"
+      puts "Winner is player 2 with a score of #{@player_2.lives.to_s}/3"
+    elsif @player_2.lives.to_s == "0"
+      puts "Winner is player 1 with a score of #{@player_1.lives.to_s}/3"
+    end
+
+    player_2_question
+  end
+
+  def player_2_question
+
+    print "Player 2, you're up. "
+    question = Question.new
+
+    print question.q_string
+
+    print "> "
+    choice = $stdin.gets.chomp.to_s
+
+    if choice == question.answer
+      puts "Great job!"
+      puts "P1: " + @player_1.lives.to_s + "/3, P2: " + @player_2.lives.to_s + "/3"
+    else
+      puts "Oh no! You got it wrong :( Now you lose a life 🔪"
+      @player_2.lose_life
+      puts "P1: " + @player_1.lives.to_s + "/3, P2: " + @player_2.lives.to_s + "/3"
+    end
+
+    if @player_1.lives.to_s == "0"
+      puts "Winner is player 2 with a score of #{@player_2.lives.to_s}/3"
+    elsif @player_2.lives.to_s == "0"
+      puts "Winner is player 1 with a score of #{@player_1.lives.to_s}/3"
+    end
+
+    player_1_question
+  end
+
+  def player_1_question
+
+    print "Player 1, you're up. "
+    question = Question.new
+
+    print question.q_string
+
+    print "> "
+    choice = $stdin.gets.chomp.to_s
+
+    if choice == question.answer
+      puts "Great job!"
+      puts "P1: " + @player_1.lives.to_s + "/3, P2: " + @player_2.lives.to_s + "/3"
+    else
+      puts "Oh no! You got it wrong :( Now you lose a life 🔪"
+      @player_1.lose_life
+      puts "P1: " + @player_1.lives.to_s + "/3, P2: " + @player_2.lives.to_s + "/3"
+    end
+
+    if @player_1.lives.to_s == "0"
+      puts "Winner is player 2 with a score of #{@player_2.lives.to_s}/3"
+    elsif @player_2.lives.to_s == "0"
+      puts "Winner is player 1 with a score of #{@player_1.lives.to_s}/3"
+    end
+
+    player_2_question
+  end
+
 end
 
-# start
-
-player_1 = Player.new("Player_1", "P1")
-player_2 = Player.new("Player_2", "P2")
-
-puts player_1.short_name
--- main.rb
+++ main.rb
@@ -1,4 +1,9 @@
 require_relative 'players'
 require_relative 'questions'
+require_relative 'game_flow'
 
 # player1 = Players::player_1
+
+new_game = GameFlow.new
+
+new_game.start
